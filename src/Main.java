694c0d4b42f42f5db83fd71a566b9ebb4eba6de9
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-27 00:21:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
-/*
-
-import models.Scooter;
-
-public class Main {
-    public static void main(String[] args) {
-        Scooter scooter = new Scooter(1);
-        scooter.year = 2021;
-        scooter.name = "Bolt";
-        scooter.colour = "green";
-
-        // 1    print year, id
-        // 2    move Scooter to "models" package to explain "protected"
-        // 3    make a Main2
-        // 4    add colour
-        System.out.println(scooter.year);
-        System.out.println(scooter.getId());
-        System.out.println(scooter.name);
-        System.out.println(scooter.colour);
-    }
-}
-
-*/
+//import models.Scooter;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scooter scooter = new Scooter(1);
+//        scooter.year = 2022;
+//        scooter.name = "Bolt";
+//        scooter.colour = "green";
+//        
+//        System.out.println(scooter.year);
+//        System.out.println(scooter.getId());
+//        System.out.println(scooter.name);
+//        System.out.println(scooter.colour);
+//        System.out.println(Scooter.MAXIMUM_SPEED);
+//    }
+//}
