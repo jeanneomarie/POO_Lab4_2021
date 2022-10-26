f6c7d7899302dde1639b7674f9c2b732cb0d132a
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-27 00:12:59 +0300

    Update MethodOverloading.java

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -8,6 +8,8 @@ public class MethodOverloading {
     }
 
     public static void main(String[] args) {
+        // static     => we can call the method directly on that class
+        // not static => we must have an object of that class in order to call the method on it
         System.out.println(test(2, 3));
         System.out.println(test(2, Byte.MAX_VALUE));
     }
