aa4d133ae896680cea35e5c5341c0991850d4feb
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-27 00:14:57 +0300

    Update Person.java

--- src/models/Person.java
+++ src/models/Person.java
@@ -15,21 +15,33 @@ public class Person {
         this.name = newName;
     }
 
-    public static void modify1(Person p) {
-        p = new Person("Modified Name");
-        System.out.println("Method modify1(Person p) printed inside: " + p.getName()); //Modified Name
+    public static void modify1(Person person1) {
+        person1 = new Person("Modified Name");
+        System.out.println("Method modify1(Person person1) printed inside: " + person1.getName()); // Modified Name
     }
 
-    public static void modify2(Person p) {
-        p.setName("Modified Name");
-        System.out.println("Method modify2(Person p) printed inside: " + p.getName());//Modified Name
+    public static void modify2(Person person2) {
+        person2.setName("Modified Name");
+        System.out.println("Method modify2(Person person2) printed inside: " + person2.getName()); // Modified Name
+    }
+
+    public static int changeNumber(int number) {
+        number = 11;
+        System.out.println(number);// 11
+        return number;
     }
 
     public static void main(String[] args) {
         Person p = new Person("Initial Person");
+
         modify1(p);
-        System.out.println("Method modify1(Person p):" + p.getName()); // Initial Name
+        System.out.println("Method modify1(Person person1):" + p.getName()); // Initial Name
+
         modify2(p);
-        System.out.println("Method modify2(Person p):" + p.getName()); // Modified Name
+        System.out.println("Method modify2(Person person2):" + p.getName()); // Modified Name
+
+        int number = 10;
+        number = changeNumber(number); // 11
+        System.out.println(number); // 10
     }
 }
